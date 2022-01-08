cfef1fedef128033f4158a11122eeb1a8031d8ce
gracentruth <63465350+gracentruth@users.noreply.github.com>
2022-01-08 16:24:04 +0900

    Initial commit

---
+++ lib/VisionDetectorViews/face_detector_view.dart
@@ -0,0 +1,75 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'camera_view.dart';
+import 'painters/face_detector_painter.dart';
+
+bool drown=false;
+
+class FaceDetectorView extends StatefulWidget {
+  @override
+  _FaceDetectorViewState createState() => _FaceDetectorViewState();
+}
+
+class _FaceDetectorViewState extends State<FaceDetectorView> {
+  FaceDetector faceDetector =
+  GoogleMlKit.vision.faceDetector(FaceDetectorOptions(
+    enableContours: true,
+    enableClassification: true,
+  ));
+
+  bool isBusy = false;
+  CustomPaint? customPaint;
+
+  @override
+  void dispose() {
+    faceDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CameraView(
+          title: 'Face Detector',
+          customPaint: customPaint,
+          onImage: (inputImage) {
+            processImage(inputImage);
+          },
+          initialDirection: CameraLensDirection.front,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            if (drown==true)
+              Text('졸지마!',style:TextStyle(fontSize: 100),
+              ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (isBusy) return;
+    isBusy = true;
+    final faces = await faceDetector.processImage(inputImage);
+    print('Found ${faces.length} faces');
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = FaceDetectorPainter(
+          faces,
+          inputImage.inputImageData!.size,
+          inputImage.inputImageData!.imageRotation);
+      customPaint = CustomPaint(painter: painter);
+    } else {
+      customPaint = null;
+    }
+    isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
